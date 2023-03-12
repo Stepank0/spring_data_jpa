be4fd991a02d495a6c9e9b246c18adf5578b5f68
Stepan Koryagin <stepanswim@gmail.com>
2023-03-12 23:05:47 +0200

    make jpa repository

---
+++ src/main/java/com/stepan/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.stepan.spring.springboot.spring_data_jpa.dao;
+
+
+
+
+
+import com.stepan.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByName(String name);
+}
